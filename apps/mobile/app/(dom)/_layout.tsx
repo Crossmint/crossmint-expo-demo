d575dfae1cfb1241fecc8ad1c7c6a1962ce67b3b
jcurbelo <robin@paella.dev>
2025-03-27 16:59:57 -0400

    changes navigation behaviour

--- apps/mobile/app/(dom)/_layout.tsx
+++ apps/mobile/app/(dom)/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function DOMLayout() {
   return (
     <Stack>
-      <Stack.Screen name="payments" options={{ title: "Checkout (DOM)" }} />
-      <Stack.Screen name="wallets" options={{ title: "Wallets (DOM)" }} />
+      <Stack.Screen name="payments" />
+      <Stack.Screen name="wallets" />
     </Stack>
   );
 }
