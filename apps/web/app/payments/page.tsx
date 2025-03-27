bb09bf8437d0f8f521ee0f4b07a93d83c4702024
jcurbelo <robin@paella.dev>
2025-03-27 15:24:03 -0400

    refactos some code

--- apps/web/app/payments/page.tsx
+++ apps/web/app/payments/page.tsx
@@ -1,34 +1,9 @@
-"use client";
-
-import { CrossmintEmbeddedCheckout } from "@crossmint/client-sdk-react-ui";
-
-const collectionId = process.env.NEXT_PUBLIC_COLLECTION_ID as string;
-
-if (!collectionId) {
-  throw new Error("NEXT_PUBLIC_COLLECTION_ID is not set");
-}
+import PaymentComponent from "@/components/PaymentComponent";
 
 export default function PaymentsPage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center mb-10">
-        <CrossmintEmbeddedCheckout
-          payment={{
-            crypto: {
-              enabled: true,
-            },
-            fiat: {
-              enabled: true,
-            },
-          }}
-          lineItems={{
-            collectionLocator: `crossmint:${collectionId}`,
-            callData: {
-              totalPrice: "0.01",
-            },
-          }}
-        />
-      </h1>
+    <div className="flex justify-center items-center h-screen">
+      <PaymentComponent />
     </div>
   );
 }
